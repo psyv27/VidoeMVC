9ffce4ad84052d2d4553b3885fe0b4b78abeb452
Emil Pashayev <93257645+psyv27@users.noreply.github.com>
2025-10-12 00:58:55 +0400

    upd

---
+++ vidoeMVC/Migrations/20250821082250_upatee12.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vidoeMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class upatee12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
